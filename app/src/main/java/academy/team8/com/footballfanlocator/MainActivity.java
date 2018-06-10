4b35b6a05138ec3fb390d1d4640546bf0d0bd0d0
ashwilliams87 <ash_williams@mail.ru>
2018-06-10 15:28:36 +0300

    added 3 buttons start activity

--- app/src/main/java/academy/team8/com/footballfanlocator/MainActivity.java
+++ app/src/main/java/academy/team8/com/footballfanlocator/MainActivity.java
@@ -2,12 +2,42 @@ package academy.team8.com.footballfanlocator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.start_activity_sign_in).setOnClickListener(this);
+        findViewById(R.id.start_activity_map).setOnClickListener(this);
+        findViewById(R.id.start_activity_send_current_location).setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.start_activity_sign_in:
+                Log.i(TAG, "onClick: opening AsyncTaskImplementation");
+                SignInActivity.start(v.getContext());
+                break;
+            case R.id.start_activity_map:
+                Log.i(TAG, "onClick: opening Hander Implementation");
+                MapActivity.start(v.getContext());
+                break;
+            case R.id.start_activity_send_current_location:
+                Log.i(TAG, "onClick: opening Hander Implementation");
+                SendLocationActivity.start(v.getContext());
+                break;
+
+        }
+
     }
 }
