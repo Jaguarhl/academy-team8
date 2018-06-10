777c5582d0d1d385c2ac409491f456026569641e
Dmitry K <dek.alpha@mail.ru>
2018-06-10 22:25:40 +0300

    recycler, registration layout, sign in layout

---
+++ app/src/main/java/academy/team8/com/footballfanlocator/interfaces/OnItemClickListener.java
@@ -0,0 +1,9 @@
+package academy.team8.com.footballfanlocator.interfaces;
+
+/**
+ * Created by dmitry on 10.06.2018.
+ */
+
+public interface OnItemClickListener {
+    void onItemClicked(int position);
+}
---
+++ app/src/main/java/academy/team8/com/footballfanlocator/model/CountryItem.java
@@ -0,0 +1,35 @@
+package academy.team8.com.footballfanlocator.model;
+
+/**
+ * Created by dmitry on 10.06.2018.
+ */
+
+public class CountryItem {
+    int countryId;
+    String flagPic;
+    String countryName;
+
+    public int getCountryId() {
+        return countryId;
+    }
+
+    public void setCountryId(int countryId) {
+        this.countryId = countryId;
+    }
+
+    public String getFlagPic() {
+        return flagPic;
+    }
+
+    public void setFlagPic(String flagPic) {
+        this.flagPic = flagPic;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+}
