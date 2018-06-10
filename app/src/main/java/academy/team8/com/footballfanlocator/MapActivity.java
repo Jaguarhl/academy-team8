8f86f5fdbd3bb1afafb517b5ddda0001ed59fbf3
info@ascott.pro <info@ascott.pro>
2018-06-10 16:45:20 +0300

    added maps support and map activity

--- app/src/main/java/academy/team8/com/footballfanlocator/MapActivity.java
+++ app/src/main/java/academy/team8/com/footballfanlocator/MapActivity.java
@@ -1,19 +1,46 @@
 package academy.team8.com.footballfanlocator;
 
+import android.app.FragmentTransaction;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MapActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
+        MapFragment mapFragment = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.mapFragment);
+        mapFragment.getMapAsync(this);
     }
 
     public static void start(Context context) {
         context.startActivity(new Intent(context, MapActivity.class));
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        ///59.93863 30.31413
+        LatLng sydney = new LatLng(59.93863, 30.31413);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
